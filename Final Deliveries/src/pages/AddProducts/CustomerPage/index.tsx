6222e4e9611fb23a8887952543e339216c0274b9
vishnusudev <vishnusudev2001@gmail.com>
2022-11-18 21:47:09 +0530

    final commit

---
+++ Final Deliveries/src/pages/AddProducts/CustomerPage/index.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react"
+import { CustomGrid } from "../../../components/DataGrid";
+import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
+import { Grid } from "@mui/material";
+
+export const CustomerTablePage = () => {
+    const [data,setData]=useState("")
+
+    const Navigate = useNavigate()
+
+    useEffect(() => {
+        getData()
+      }, []);
+
+    async function getData(){
+
+            const response = await fetch("API adsress");
+            
+            
+            var data = await response.json();
+            
+            console.log(data);
+            setData(data)
+
+            
+            }
+
+
+    const columnDefs = [
+        { field: 'Sno' },
+        { field: 'CustomerName' },
+        { field: 'CustomerEmail' },
+        { field: 'WarehouseName' },
+        { field: 'Phone' },
+        { field: 'Country' },
+    ];
+
+    const rowDef = [
+        { Sno: "1", CustomerName: "Sudev", CustomerEmail: "sudev@gmail.com", WarehouseName: 'Ware', Phone: '9876543210', Country: 'India' },
+        { Sno: "1", CustomerName: "Sudev", CustomerEmail: "sudev@gmail.com", WarehouseName: 'Ware', Phone: '9876543210', Country: 'India' },
+        { Sno: "1", CustomerName: "Sudev", CustomerEmail: "sudev@gmail.com", WarehouseName: 'Ware', Phone: '9876543210', Country: 'India' },
+        { Sno: "1", CustomerName: "Sudev", CustomerEmail: "sudev@gmail.com", WarehouseName: 'Ware', Phone: '9876543210', Country: 'India' },
+        { Sno: "1", CustomerName: "Sudev", CustomerEmail: "sudev@gmail.com", WarehouseName: 'Ware', Phone: '9876543210', Country: 'India' }
+    ]
+
+    return (
+        <>
+            <Grid container spacing={2} style={{ marginLeft: "15px" }}>
+                <Grid xs={10}>
+                    <h2 style={{ fontSize: "20px", fontFamily: "sans-serif" }}>Customers</h2>
+                </Grid>
+                <Grid xs={2}>
+                    <Button variant="contained" style={{ marginTop: "10px" }} onClick={() => Navigate("/CustomerPage")}>Add Customer</Button>
+                </Grid>
+            </Grid>
+
+            <CustomGrid columnDefs={columnDefs} rowData={data} />
+        </>
+    )
+}
---
+++ Final Deliveries/src/pages/Register/index.tsx
@@ -0,0 +1,60 @@
+import { Avatar, Button, TextField } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import { Grid } from '@mui/material';
+import React, { useContext, useState } from "react"
+import { Link, useNavigate} from "react-router-dom";
+import { UserContext } from "../../context";
+
+export const Register=()=>{
+    const [Remail, setRemail] = useState("");
+    const [Rpass, setRpass] = useState("");
+    const [Rname, setRname] = useState("");
+    const navigate=useNavigate();
+    const pstyle={padding:30 ,height:'50vh', width:280,margin:'40px auto'}
+    const{setUser}:any=useContext(UserContext)
+
+    function handleRegister() {
+        if(Remail!=="" && Rpass!=="" && Rname!==""){
+            // fetch ("API Address", {
+            //     method: "POST",
+            //     body: JSON.stringify({
+            //       email: Remail,
+            //       password:Rpass,
+            //       name:Rname
+            //    }),
+            //    })
+            //  .then((response) => response.json())
+            //  .then((result) => {
+            //      if(result.message === "SUCCESS"){
+            //      alert("Registeration Successfull");
+            //      setUser(true);
+                 
+            //     } 
+            //     else {
+            //         alert("Please check your login information.");
+            //     }
+            //    });
+            setUser(true);
+        
+        }
+        else{
+            alert("Enter correct details")
+        }
+    }
+    return(
+        <Grid>
+            <Paper elevation={10} style={pstyle}>
+                <Grid>
+                {/* <Avatar style={astyle}><LockOutlinedIcon/></Avatar> */}
+                <h2 >Register</h2>
+                </Grid>
+                <TextField  onChange={(event) => {setRemail(event.target.value);}}  label='Email' id='outlined-basic' fullWidth/><br/><br/>
+                <TextField  onChange={(event) => {setRpass(event.target.value);}}  label='Password'  type='password'id='outlined-basic' fullWidth/><br/><br/>
+                <TextField  onChange={(event) => {setRname(event.target.value);}}  label='Username' id='outlined-basic' fullWidth/><br/><br/>
+                <Button onClick={() => {handleRegister()}} type='submit' fullWidth>SignUp</Button>
+                Already a user?<Link to="/Login"> Login</Link>
+                
+            </Paper>
+        </Grid>
+    )
+}
